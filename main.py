3d9676d5e8859f5e0647d91eb19843f909e03db0
Nathan Gilbert <natedrewg@gmail.com>
2023-11-29 13:17:14 -0500

    added necessities

--- main.py
+++ main.py
@@ -2,34 +2,39 @@ import xgboost as xgb
 import pandas as pd
 from pyscript import Element
 from js import document, window
+import numpy as np
 
 classes = ["0","1","2"]
 
 loaded_model = xgb.Booster()
 loaded_model.load_model("xgb_model.json")
 
-def get_predictions():
+def transformPassword(pWord):
+  length = len(pWord)
+  charNum = 0
+  if (pWord.isalpha()==0 & pWord.isdigit()==0):
+      charNum = 1
+  capLetter = 0
+  if (pWord.upper() != pWord and pWord.lower() != pWord):
+    capLetter = 1
+  specialChar = 1
+  if(pWord.isalnum()==1):
+    specialChar = 0
+  return np.array([length, charNum, capLetter, specialChar]).reshape(1, -1)
+
+def get_prediction():
   try:
-    sl = str(Element("sl").value)
-    sw = str(Element("sw").value)
-    pl = str(Element("pl").value)
+    pas = str(Element("password").value)
+
   except:
     # window.alert("Please Enter valid values!")
     return 0
   
   sample_test_record = pd.DataFrame([{
-      'sepal length (cm)': float(Element("sl").value),
-      'sepal width (cm)': float(Element("sw").value),
-      'petal length (cm)': float(Element("pl").value),
+      transformPassword(pas)
   }])
   prediction = loaded_model.predict(xgb.DMatrix(sample_test_record))
 
-  # predicted_class = "SETOSA"
-
-  predicted_class = classes[int(prediction.argmax())]
-  document.querySelector("#iris_image").src = f"./images/{predicted_class}.png"
-  document.querySelector("#prediction_result").innerHTML = f"The iris flower for the input data is : <b>{predicted_class}</b>"
-
   # print("Predicted class for the above sample_test_record:",predicted_class)
   
-  return predicted_class
+  return prediction
